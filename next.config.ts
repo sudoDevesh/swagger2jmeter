b6cf5159015a1f0fa6a10cd0dc7894f0744c272d
Devesh Korde <DeveshK@centiro.se>
2025-11-24 09:24:25 +0530

    github workflow

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/swagger2jmeter',
+  assetPrefix: '/swagger2jmeter/',
 };
 
 export default nextConfig;
