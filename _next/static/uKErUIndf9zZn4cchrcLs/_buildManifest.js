213edd465dda56cc1bca643df4699ebb96652e27
sudoDevesh <sudoDevesh@users.noreply.github.com>
2025-11-24 04:03:47 +0000

    deploy: 179ec998469117c127e8cb0dcd4421ea6dcf3380

---
+++ _next/static/uKErUIndf9zZn4cchrcLs/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/swagger2jmeter//_next/:path+",
+        "destination": "/swagger2jmeter/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
